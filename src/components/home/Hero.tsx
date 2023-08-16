17198ac4deaf490daf7879a3f7f7857170abf952
Lance <jiexi@Lances-MacBook-Air.local>
2023-08-16 19:13:00 +0200

    updated Home component

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,17 +5,17 @@ function Hero() {
   return (
     <div className="flex items-center w-full  lg:max-w-[1336px] mx-auto container px-4 xl:px-0 flex-col">
       <div className="">
-        <h1 className="text-2xl lg:text-[64px] font-bold tracking-[0.4px] leading-10 lg:leading-[86px] bg-gradient-to-b from-[#194c33] to-black text-transparent bg-clip-text select-none">
-          STYLE YOUR HOUSE WITH THE BEST LOOKING FURNITURE
+        <h1 className="text-2xl lg:text-[64px] font-bold tracking-[0.4px] leading-10 lg:leading-[86px] bg-gradient-to-b from-[#194c33] to-black text-transparent bg-clip-text select-none uppercase">
+          Style your home with modern and stylish furniture
         </h1>
       </div>
       <div className="flex gap-6 mt-[20px] lg:mt-[40px] flex-col lg:flex-row lg:gap-10">
         <div className="flex-1">
-          <p className="leading-[22px] text-black text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio est
-            commodi omnis laborum exercitationem veritatis animi, vitae aliquam
-            qui quasi! Perspiciatis commodi, itaque animi laudantium odio
-            maiores? Neque, eveniet dolore?.
+          <p className="text-sm leading-[22px] text-black lg:text-base">
+            Shop modern and high quality home and office furniture at reasonable
+            prices. At Willow Inter Designs, we guarantee efficient and
+            satisfiying services to make your purchasing easy and simple,
+            leaving your home looking stylish.
           </p>
         </div>
         <div className="flex-1 flex justify-end">
