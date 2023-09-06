541783dbd1999479d233727c521113743d27e4e2
Lance <jiexi@Lances-MacBook-Air.local>
2023-09-06 21:52:52 +0200

    updated supabaseServer file

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -2,6 +2,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
 export default async function supabaseServerComponentClient() {
+    const cookieStore = cookies();
 
-    return createServerComponentClient({ cookies });
+    return createServerComponentClient({ cookies: () => cookieStore });
 }
