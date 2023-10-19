413a9d02186290b03234f2dbde07e4c2cab7d222
Lance <jiexi@Lances-MacBook-Air.local>
2023-10-20 01:21:05 +0200

    update the request, removed cosole logging

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -3,116 +3,139 @@ import supabaseServerComponentClient from "@/lib/supabaseServer";
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-
-
-
-const postUrl = "https://payments.yoco.com/api/checkouts"
-
 const convertToCents = (amount: number) => amount * 100
 
 export async function POST(request: NextRequest) {
-    const supabase = await supabaseServerComponentClient()
 
-    const { data: { user }, } = await supabase.auth.getUser()
+    try {
+        const supabase = await supabaseServerComponentClient()
 
-    //    check if there is a user
-    if (!user) {
-        throw new Error("You must be logged in to make a payment")
-    }
+        const { data: { user }, } = await supabase.auth.getUser()
 
-    const cart = await prisma.cart.findFirst({
-        where: {
-            userId: user.id,
-        },
-        include: {
-            items: {
-                include: {
-                    Product: true,
+        //    check if there is a user
+        if (!user) {
+            throw new Error("You must be logged in to make a payment")
+        }
+
+        // get the cart
+        const cart = await prisma.cart.findFirst({
+            where: {
+                userId: user.id,
+            },
+            include: {
+                items: {
+                    include: {
+                        Product: true,
+                    },
                 },
             },
-        },
-    })
+        })
 
-    console.log(cart)
-
-    // check if there is a cart
-    if (!cart) {
-        throw new Error("Add items to cart to make a payment")
-    }
-
-    // compare cart items with product items and check if the product is still available
-    const cartItems = cart.items.map(async (item) => {
-        const isInStock = await prisma.product.findUnique({
-            where: {
-                id: item.productId
-            },
-        });
-
-        if (!isInStock) {
-            throw new Error("One or more products in cart are currently out of stock")
+        // check if there is a cart
+        if (!cart) {
+            throw new Error("Add items to cart to make a payment")
         }
 
-        if (isInStock.quantity > item.quantity) {
-            return item
+        // compare cart items with product items and check if the product is still available
+        const cartItems = cart.items.map(async (item) => {
+            const isInStock = await prisma.product.findUnique({
+                where: {
+                    id: item.productId
+                },
+            });
+
+            if (!isInStock) {
+                throw new Error("One or more products in cart are currently out of stock")
+            }
+
+            if (isInStock.quantity > item.quantity) {
+                return item
+            }
+        })
+
+
+        // total amount of the cart
+        const totalAmount = cart.items.reduce((acc, item) => {
+            return acc + item.quantity * item.Product.price
+        }, 0)
+
+
+        let lineItems = []
+
+        cart.items.forEach((item) => {
+            lineItems.push({
+                displayName: item.Product.name,
+                quantity: item.quantity,
+                pricingDetails: {
+                    price: convertToCents(item.Product.price),
+                },
+            })
+        })
+
+        // setup headers and data for the request
+        const discount = 0
+        const totalTaxAmount = totalAmount * 0.15
+        const subTotalAmount = totalAmount + discount + totalTaxAmount
+        const amount = convertToCents(subTotalAmount)
+        const orderKey = crypto.randomUUID()
+
+        const headers = {
+            "Authorization": `Bearer ${process.env.YOCO_SECRET_KEY}`,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Idempotency-Key": orderKey,
         }
-    })
 
-    console.log(cartItems)
-
-    // us reduce to calculate the total amount of the cart
-    const totalAmount = cart.items.reduce((acc, item) => {
-        return acc + item.quantity * item.Product.price
-    }, 0)
-
-    const orderKey = crypto.randomUUID()
-
-    console.log(totalAmount)
-
-    const headers = {
-        "Authorization": `Bearer sk_test_a175633b5mm2Kv575704cf9911cd`,
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        // Idempotency-Key create uuid
-        "Idempotency-Key": orderKey,
-    }
-
-    // create order
-    const order = await prisma.order.create({
-        data: {
-            userId: user.id,
-            status: "PENDING",
-            orderKey: orderKey,
-            total: totalAmount,
-            OrderItem: {
-                //    connect to product
-                create: cart.items.map((item) => ({
-                    quantity: item.quantity,
-                    Product: {
-                        connect: {
-                            id: item.productId,
+        // create order
+        const order = await prisma.order.create({
+            data: {
+                userId: user.id,
+                status: "PENDING",
+                orderKey: orderKey,
+                total: totalAmount,
+                OrderItem: {
+                    //    connect to product
+                    create: cart.items.map((item) => ({
+                        quantity: item.quantity,
+                        Product: {
+                            connect: {
+                                id: item.productId,
+                            },
                         },
-                    },
-                })),
+                    })),
+                },
             },
-        },
-    })
+        })
 
-    const reponse = await axios.post(postUrl, {
-        amount: convertToCents(totalAmount),
-        currency: process.env.PAYMENT_CURRENCY,
-        cancelUrl: `${process.env.DOMAIN_URL}/checkout?cancel=1`,
-        successUrl: `${process.env.DOMAIN_URL}/checkout?succes=1`,
 
-    }, { headers: headers })
+        const reponse = await axios.post(`${process.env.YOCO_URL}`, {
+            amount: amount,
+            currency: process.env.PAYMENT_CURRENCY,
+            lineItems: lineItems,
+            totalDiscount: discount,
+            totalTaxAmount: totalTaxAmount,
+            subtotalAmount: subTotalAmount,
+            cancelUrl: `${process.env.DOMAIN_URL}/checkout?cancel=1`,
+            successUrl: `${process.env.DOMAIN_URL}/checkout?succes=1`,
 
-    await prisma.cart.delete({
-        where: {
-            id: cart.id,
-        },
-    })
-    //   return to the client
-    return NextResponse.json({
-        url: reponse.data.redirectUrl,
-        id: reponse.data.id,
-    })
+        }, { headers: headers })
+
+        // delete cart
+        await prisma.cart.delete({
+            where: {
+                id: cart.id,
+            },
+        })
+
+        return NextResponse.json({
+            url: reponse.data.redirectUrl,
+            id: reponse.data.id,
+        })
+    } catch (error) {
+        // return error message
+        return NextResponse.json({
+            error: "Something went wrong, please try again"
+        })
+    }
+
 }
