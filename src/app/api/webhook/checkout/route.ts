2e6827de23b1f165369e74ceb185910927f18dbe
Lance <jiexi@Lances-MacBook-Air.local>
2023-10-23 19:05:36 +0200

    removed console logs

--- src/app/api/webhook/checkout/route.ts
+++ src/app/api/webhook/checkout/route.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import {headers} from "next/headers"
+import { headers } from "next/headers"
 import * as crypto from "crypto"
 import { prisma } from "@/lib/prisma";
 
-const secret = process.env.YOCO_WEBHOOK_SECRET
 export async function POST(req: NextRequest, res: NextResponse) {
-     // get raw body
+    // get raw body
     const body = await req.text()
     const id = headers().get("webhook-id")
     const timestamp = headers().get("webhook-timestamp")
@@ -13,27 +12,24 @@ export async function POST(req: NextRequest, res: NextResponse) {
     //create signedContent
     const signedContent = `${id}.${timestamp}.${body}`
 
-     // determine expected signature
-    const secretBytes = new Buffer(secret.split('_')[1], "base64");
+    // determine expected signature
+    const secretBytes = new Buffer(process.env.YOCO_WEBHOOK_SECRET!.split('_')[1], "base64");
 
     const expectedSignature = crypto
-    .createHmac('sha256', secretBytes)
-    .update(signedContent)
-    .digest('base64');
+        .createHmac('sha256', secretBytes)
+        .update(signedContent)
+        .digest('base64');
 
     // compare signatures
 
     const signature = headers().get('webhook-signature')?.split(' ')[0].split(',')[1]
-    console.log(signature)
-    if (crypto.timingSafeEqual(Buffer.from(expectedSignature), Buffer.from(signature??""))) {
+    if (crypto.timingSafeEqual(Buffer.from(expectedSignature), Buffer.from(signature ?? ""))) {
         const data = JSON.parse(body)
-        console.log(data)
 
         // check if payment is successful
         if (data.type === "payment.succeeded") {
-            console.log(data.payload.metadata.checkoutId)
             const order = await prisma.order.findUnique({
-                where:{
+                where: {
                     id: data.payload.metadata.orderId
                 }
             })
@@ -53,9 +49,9 @@ export async function POST(req: NextRequest, res: NextResponse) {
         }
 
 
-        return new NextResponse(null, {status: 200})
+        return new NextResponse(null, { status: 200 })
     }
 
     // do not process webhook event
-    return NextResponse.json({message: "not received"})
+    return NextResponse.json({ message: "not received" })
 }
