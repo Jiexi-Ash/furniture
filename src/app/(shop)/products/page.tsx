d897b3aeca1a06c511df3a3b07e469bd5349727e
Lance <jiexi@Lances-MacBook-Air.local>
2023-10-24 17:14:17 +0200

    revalidate after 1 hour

--- src/app/(shop)/products/page.tsx
+++ src/app/(shop)/products/page.tsx
@@ -3,6 +3,7 @@ import Products from "@/components/products/Products";
 import MainLayout from "@/components/layout/MainLayout";
 import { getProducts } from "@/app/_actions/products";
 
+export const revalidate = 3600;
 export default async function ProductsPage() {
   const products = await getProducts();
   return (
