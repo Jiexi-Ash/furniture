3fbf8b92dfd30dbd3623ada7c46691685f243710
Lance <jiexi@Lances-MacBook-Air.local>
2023-10-24 17:44:07 +0200

    added some error handling

--- src/components/checkout/PaymentBtn.tsx
+++ src/components/checkout/PaymentBtn.tsx
@@ -2,15 +2,30 @@
 import React from "react";
 import { Button } from "../ui/button";
 import axios from "axios";
+import { AxiosError } from "axios";
+import { useToast } from "../ui/use-toast";
 
 function PaymentBtn() {
+  const { toast } = useToast();
   const handlePayment = async () => {
     try {
       const { data } = await axios.post("/api/checkout");
       //  redirect to url
       window.location.href = data.url;
     } catch (error) {
-      console.log(error);
+      const axiosError = error as AxiosError;
+      if (!axios.isAxiosError(axiosError)) {
+        toast({
+          description: "Something went wrong, please try again",
+        });
+      } else {
+        toast({
+          title: "Payment Failed",
+          description:
+            (axiosError?.response?.data as { message?: string })?.message ||
+            "Unknown error occurred",
+        });
+      }
     }
   };
   return (
